e592f2a9c9ea28544be46248966dd1eb6f0f7e28
Víctor Pérez Barranco <victorperezbarranco@MacBook-Air-de-Victor.local>
2024-11-03 21:20:35 +0100

    Blog Feature and first post

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,29 +2,36 @@ import { TECHNOLOGIES } from './technologies'
 
 export interface Project {
 	proyectId: string
-	coverImage: string
 	title: string
+	logo?: string
+	coverImage: string
 	description: Array<string>
 	technologies: { name: string; color: string; Icon: any }[]
 	repo?: string
 	link?: string
+	diseño?: string
 	images: Array<string>
+	date?: string
 }
 
 export const PROJECTS: Project[] = [
 	{
 		proyectId: 'Portfolio',
-		coverImage: '/portfolio-preview.webp',
 		title: 'Portfolio Vipeba',
+		logo: '/logo.webp',
+		coverImage: '/portfolio-preview.webp',
 		description: [
 			'Porfolio de presentación como desarrollador de software.',
-			'Diseñado en Figma buscando un enfoque moderno y atractivo.',
-			'Desarrollado con Astro y Tailwind CSS aprovechando su capacidad para generar sitios estáticos rápidos y eficientes.'
+			'Diseñado buscando un enfoque moderno y llamativo.',
+			'Muestra la experiencia laboral, CV, proyectos y contenido profesional.'
 		],
 		technologies: [TECHNOLOGIES.FIGMA, TECHNOLOGIES.ASTRO, TECHNOLOGIES.TAILWIND],
 		link: 'https://vipeba.dev',
 		repo: 'https://github.com/VictorPB/Portfolio',
-		images: []
+		diseño:
+			'https://www.figma.com/design/eP9KnXSCUVSPVzJb0mcM2W/Portfolio-Vipeba?node-id=19-1772&m=dev',
+		images: [],
+		date: '22/09/2024'
 	}
 ]
 
